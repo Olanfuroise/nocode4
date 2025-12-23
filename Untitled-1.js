0a830f081fb2cd9866666bd74fdda2565d7845cf
Olanfuroise <nolanhouizot@gmail.com>
2025-12-23 13:54:24 +0100

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -438,6 +438,9 @@ loadData();
 renderQuests();
 renderHistory();
 renderShop();
-renderDailyQuests();
+renderDailyQuests(); 
+
+async function testMinecraft() { alert("Test en cours..."); try { const response = await fetch("https://minecraft-api-production-39aa.up.railway.app/cmd", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ command: "say TEST depuis ton site NoCoins", key: "123456789" }) }); const text = await response.text(); alert("Réponse API : " + text); } catch (err) { alert("Erreur : API inaccessible"); } }
+
 
 
